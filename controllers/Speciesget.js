b7ececcc99278ee008531ea085b34947dbaf627c
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-10-13 20:11:14 -0600

    geospecies + geoindicators are streaming

--- controllers/Speciesget.js
+++ controllers/Speciesget.js
@@ -6,30 +6,77 @@ const QueryStream = require('pg-query-stream')
 // const SequelizeStream = require('../config/sequelize-stream')
 const  db  = require('../config/database')
 const {Pool} = require('pg')
+const dataGap = require('../models/dataGap')
 const pool = new Pool({
   connectionString:process.env.DBSTR
 })
 
+testparams = {
+  'primaryKey':"1109211155239312011-09-01",
+  "EcologicalSiteId":"R023XY200OR"
+}
+
+let sql = `
+SELECT "dataHeader".*, "geoSpecies".* 
+FROM (
+  SELECT * FROM "dataHeader" AS "dataHeader" 
+  ) 
+AS "dataHeader" 
+LEFT OUTER JOIN "geoSpecies" AS "geoSpecies" 
+ON "dataHeader"."PrimaryKey" = "geoSpecies"."PrimaryKey"
+`
+
 exports.getSpecies = (req, res, next) =>{
+  // console.log(req.query)
+  let values = []
+  if (Object.keys(req.query).length!==0){
+    let head = "WHERE "
+    // let params = [req.query]
+    let list = []
+    
+    let count = 1
+    for(const [key,value] of Object.entries(req.query)){
+      if(Array.isArray(value)){
+        for (i = 0; i<value.length; i++){
+          // console.log(value[i])
+          // temp = `${key} = ${value[i]}`
+          temp = `"dataHeader"."${key}" = $${count}`
+          count+=1
+          values.push(value[i])
+          list.push(temp)
+        }
+      } else {
+        temp = `"dataHeader"."${key}" = $${count}`
+        values.push(value)
+        list.push(temp)
+        
+      }
+      
+    }
+    sql = sql + head + list.join(" AND ")
+
+  }
+  
   pool.connect((err, client, release)=>{
     if(err){
       return console.error("error ")
     }
-    const query = new QueryStream('SELECT * FROM "geoSpecies"')
-    const stream = client.query(query)
-    // client.query('SELECT * FROM "geoSpecies" LIMIT 1', (err, result)=>{
-    //   release()
-    //   if (err){
-    //     return console.error("error #2")
-    //   }
-    //   console.log(result.rows)
-    // })
-    stream.on('end',release)
-    stream.pipe(JSONStream.stringify()).pipe(res)
+    if (Object.keys(req.query).length!==0){
+      const query = new QueryStream(sql, values)
+      const stream = client.query(query)
+      stream.on('end',release)
+      stream.pipe(JSONStream.stringify()).pipe(res)
+    } else{
+      const query = new QueryStream(sql)
+      const stream = client.query(query)
+
+      stream.on('end',release)
+      stream.pipe(JSONStream.stringify()).pipe(res)
+    }
+    
+    
   })
-    // console.log("yer here",  )
 
 
-  
 }
 
--- controllers/geoIndicatorget.js
+++ controllers/geoIndicatorget.js
@@ -2,28 +2,108 @@
 const Header = require('../models/dataHeader')
 const geoIndicators= require('../models/geoIndicators')
 
+const JSONStream = require('JSONStream')
+const QueryStream = require('pg-query-stream')
+// const SequelizeStream = require('../config/sequelize-stream')
+const  db  = require('../config/database')
+const {Pool} = require('pg')
+const dataGap = require('../models/dataGap')
+const pool = new Pool({
+  connectionString:process.env.DBSTR
+})
+
+let sql = `
+SELECT "dataHeader".*, "geoIndicators".* 
+FROM (
+  SELECT * FROM "dataHeader" AS "dataHeader" 
+  ) 
+AS "dataHeader" 
+LEFT OUTER JOIN "geoIndicators" AS "geoIndicators" 
+ON "dataHeader"."PrimaryKey" = "geoIndicators"."PrimaryKey"
+`
+
+
+
+// SEQUELIZE CONTROLLER
+// exports.getGeoInd = (req, res, next) =>{
+//   let whichgeoInd = req.params
+
+//   Header.findAll({
+//     where: whichgeoInd,
+//     include: [
+//       {
+//         model: geoIndicators
+//       }
+//     ],
+
+//     limit:1,
+//     raw:true,
+
+//   })
+
+//   .then( r => {
+//     res.status(200).json(r)
+//   })
+//   .catch(err=>{console.log(err)})
+// }
+
+// STREAMING CONTROLLER
 
 exports.getGeoInd = (req, res, next) =>{
-  let whichgeoInd = req.params
-
-  Header.findAll({
-    where: whichgeoInd,
-    include: [
-      {
-        model: geoIndicators
+  //parsing URL query parameters IF they exist
+  let values = []
+  if (Object.keys(req.query).length!==0){
+    let head = "WHERE "
+    // let params = [req.query]
+    let list = []
+    
+    let count = 1
+    for(const [key,value] of Object.entries(req.query)){
+      if(Array.isArray(value)){
+        for (i = 0; i<value.length; i++){
+          // console.log(value[i])
+          // temp = `${key} = ${value[i]}`
+          temp = `"dataHeader"."${key}" = $${count}`
+          count+=1
+          values.push(value[i])
+          list.push(temp)
+        }
+      } else {
+        temp = `"dataHeader"."${key}" = $${count}`
+        values.push(value)
+        list.push(temp)
+        
       }
-    ],
+      
+    }
+    sql = sql + head + list.join(" AND ")
 
-    // limit:100,
-    raw:true,
+  }
+  
+  pool.connect((err, client, release)=>{
+    if(err){
+      return console.error("error ")
+    }
+    if (Object.keys(req.query).length!==0){
+      const query = new QueryStream(sql, values)
+      const stream = client.query(query)
+      stream.on('end',release)
+      stream.pipe(JSONStream.stringify()).pipe(res)
+    } else{
+      const query = new QueryStream(sql)
+      const stream = client.query(query)
 
+      stream.on('end',release)
+      stream.pipe(JSONStream.stringify()).pipe(res)
+    }
+    
+    
   })
 
-  .then( r => {
-    res.status(200).json(r)
-  })
-  .catch(err=>{console.log(err)})
 }
+
+
+
 
 exports.getCleanGeoind = (req, res, next) =>{
   let whichgeoInd = req.params
