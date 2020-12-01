8f14b1c16ddf8093a4efa644311aefda24127984
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-12-01 13:58:12 -0700

    row by row value check

--- controllers/posttest.js
+++ controllers/posttest.js
@@ -7,15 +7,37 @@ const fs = require('fs')
 
 exports.postTest = (req, res, next) =>{
   dataTest.sync()
- 
+  // console.log(req.body)
   try{
-      Object.entries(req.body).forEach( i => 
-        // here each bulk insert would start
-        // needs field check
-        dataTest.create(i[1])
-        )
       
-      // const data = dataTest.create(req.body)
+      Object.entries(req.body).forEach( (value,index) => 
+        // here each bulk insert would start
+        // row by row from streaming json (checking if value exists)
+        dataTest.findOne({where:{parameter_set:parameter_count(value)}})
+        .then(e=>{
+            
+            return `value "${parameter_count(value)}" found; skipping`
+          })
+        .catch(er=>{
+            console.log(index)
+            dataTest.create(value[1])
+          })
+        )
+      //   dataTest.findOrCreate({
+      //     where: {
+      //       parameter_set:parameter_count(value)
+      //     },
+      //     defaults: value[1]
+      //   })
+      //   .then(found => {
+      //     return `value "${parameter_count(value)}" found; skipping`
+      //   })
+      //   .catch(notfound=>{
+      //     if (notfound){
+      //       console.log("entry added")
+      //     }
+      //   })
+      // )
       res.status(200).send( "done")
 
   } catch(error){
@@ -24,4 +46,13 @@ exports.postTest = (req, res, next) =>{
 
   }
   
+}
+const parameter_count = (obj) =>{
+  for (let i=0; i<obj.length;i++){
+    if (obj[1].hasOwnProperty('parameter_set')){
+      return obj[1].parameter_set
+    } else {
+      return 'No parameter_set field'
+    }
+  }
 }
