5b8a684bc86dd614956a8b1c49d0ff0651bc7e8c
Kris! <42949874+krstphrrr@users.noreply.github.com>
2021-01-20 17:43:41 -0700

    added coord concat + streaming results

--- controllers/headerget.js
+++ controllers/headerget.js
@@ -98,67 +98,223 @@ exports.postHeader = (req, res, next) =>{
       res.status(400).send(error)
     }
 }
+let pairUp = (list)=>{
+  let output = []
+  list.forEach((val,index)=>{
+    if(index<(list.length-1)&&(index%2)===0){
+      output.push([val,list[index+1]])
+    }
+  })
+  return output
+}
+
+let coordPair = (list) =>{
+  let pairList = ''
+  let subPairs = ''
+  let startPoint = ''
+  for(let i in list){
+    if(list.length===1){
+      for(let j in list[i]){
+        subPairs+=list[i][j]
+      }
+      pairList+=`${subPairs} `
+      subPairs = ''
+    } else {
+      if (i==0){
+        for (let j in list[i]){
+          if(j==list[i].indexOf(list[i][list[i].length-1])){
+            subPairs+=`${list[i][j]} `
+          } else {
+            subPairs+=`${list[i][j]} `
+          }
+        }
+        startPoint+=`${subPairs} `
+        subPairs = ''
+      }
+      if (i==list.indexOf(list[list.length-1])){
+        for(let j in list[i]){
+
+          if(j==list[i].indexOf(list[i][list[i].length-1])){
+            subPairs+=`${list[i][j]} `
+          } else {
+            subPairs+=`${list[i][j]} `
+          }                
+        }
+        pairList+=`${subPairs}`
+        subPairs = ''
+      } else {
+        for(let j in list[i]){
+          if(j==list[i].indexOf(list[i][list[i].length-1])){
+            subPairs+=`${list[i][j]} `
+          } else {
+            subPairs+=`${list[i][j]} `
+          } 
+        }
+        pairList+=`${subPairs}, `
+        subPairs = '' 
+        // paramList+=`"${tmpData[i]}", `
+      }
+    }
+    
+  }
+  return pairList
+}
 
 exports.getHeaderCoords = (req, res, next) =>{
+  sql = `
+    SELECT * 
+      FROM 
+    `
   for(const [key,value] of Object.entries(req.query)){
     // console.log(value)
     let bufferObj = Buffer.from(value, 'base64')
     let decoded = bufferObj.toString("utf-8")
     let usefulCoords = decoded.split(",").map(Number)
     console.log(usefulCoords)
+    let pre = pairUp(usefulCoords)
+    let finalcoords = `header_json('${coordPair(pre)}')`
+    sql = sql + finalcoords
+    console.log(sql)
+
+    pool.connect((err,client, release)=>{
+      res.contentType('application/json')
+      if(err){
+        return console.error("error ")
+      }
+      if (Object.keys(req.query).length!==0){
+  
+        const query = new QueryStream(sql)
+        const stream = client.query(query)
+        stream.on('end',release)
+        stream.pipe(JSONStream.stringify()).pipe(res)
+      } else {
+        const query = new QueryStream(sql)
+        const stream = client.query(query)
+  
+        stream.on('end',release)
+        stream.pipe(JSONStream.stringify()).pipe(res)
+      }
+    })
   }
-  // sql = `
-  //   SELECT * 
-  //     FROM "dataHeader"
-  //   `
-  // let values = []
-  // let head = "WHERE "
-  // let defaultJoinVerb = " AND "
-  // if (Object.keys(req.query).length!==0){
-  //   let list = []
-  //   let count = 1
 
-  //   for(const [key,value] of Object.entries(req.query)){
-      
-  //     let trick = value.split(",")
-      
-  //     if(Array.isArray(trick)){
-  //       defaultJoinVerb = " OR "
-  //       for (i = 0; i<trick.length; i++){
-  //         temp = `"dataHeader"."${key}" = $${count}`
-  //         count+=1
-  //         values.push(trick[i])
-  //         list.push(temp)
-  //       }
-  //     } else {
-  //       defaultJoinVerb = " AND "
-  //       temp = `"dataHeader"."${key}" = $${count}`
-  //       count+=1
-  //       values.push(value)
-  //       list.push(temp)
-  //     }
-  //   }
-  //   console.log(list)
-  //   sql = sql + head + list.join(defaultJoinVerb)
-  //   console.log(sql)
-  // }
-  // pool.connect((err,client, release)=>{
-  //   res.contentType('application/json')
-  //   if(err){
-  //     return console.error("error ")
-  //   }
-  //   if (Object.keys(req.query).length!==0){
 
-  //     const query = new QueryStream(sql, values)
-  //     const stream = client.query(query)
-  //     stream.on('end',release)
-  //     stream.pipe(JSONStream.stringify()).pipe(res)
-  //   } else {
-  //     const query = new QueryStream(sql)
-  //     const stream = client.query(query)
+/*
+BEGIN 
 
-  //     stream.on('end',release)
-  //     stream.pipe(JSONStream.stringify()).pipe(res)
-  //   }
-  // })
+	 RETURN json_build_object(
+    'type', 'FeatureCollection', 
+    'features', json_agg(
+        json_build_object(
+            'type',       'Feature',
+            'id',         gs."PrimaryKey", 
+            'geometry',   postgis.ST_AsGeoJSON(dh."wkb_geometry")::json,
+            'properties', json_build_object(
+                -- list of fields
+                'id', gs."PrimaryKey", 
+				'EcologicalSiteId', dh."EcologicalSiteId", 
+				'Project', dh."ProjectKey",
+				'isPublic', gs."Public"
+            )
+        )
+    )
+)
+FROM 
+	public."dataHeader" as dh
+INNER JOIN 
+	public."geoSpecies" as gs
+ON dh."PrimaryKey" = gs."PrimaryKey"
+
+WHERE postgis.ST_Intersects(
+	dh.wkb_geometry,
+	postgis.ST_MakePolygon(
+		postgis.ST_SetSRID(
+			postgis.ST_GeomFromText(
+				format('LINESTRING(%s)', VARIADIC "coords")
+			)::postgis.geometry, 
+	4326))) = 't' AND gs."Public" = "isPublic";
+END;
+
+
+CREATE OR REPLACE FUNCTION header_json(VARIADIC coords character varying[])
+   RETURN json as 
+$$
+BEGIN
+(SELECT row_to_json(dh) 
+
+FROM 
+	public."dataHeader" as dh
+
+WHERE postgis.ST_Intersects(
+	dh.wkb_geometry,
+	postgis.ST_MakePolygon(
+		postgis.ST_SetSRID(
+			postgis.ST_GeomFromText(
+				format('LINESTRING(%s)', VARIADIC "coords")
+			)::postgis.geometry, 
+	4326))) = 't')
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE FUNCTION header_json(VARIADIC coords character varying[])
+RETURNS SETOF "dataHeader"
+AS
+$body$
+
+(SELECT *
+
+FROM 
+	public."dataHeader" as dh
+
+WHERE postgis.ST_Intersects(
+	dh.wkb_geometry,
+	postgis.ST_MakePolygon(
+		postgis.ST_SetSRID(
+			postgis.ST_GeomFromText(
+				format('LINESTRING(%s)', VARIADIC "coords")
+			)::postgis.geometry, 
+	4326))) = 't')
+	
+$body$
+language sql
+
+
+
+
+
+
+
+
+[-98.547894, 43.097202,-100.549706, 43.057674,-98.845461, 44.753565,-98.547894, 43.097202]
+
+
+
+
+
+
+
+(SELECT row_to_json(dh) 
+
+FROM 
+	public."dataHeader" as dh
+
+WHERE postgis.ST_Intersects(
+	dh.wkb_geometry,
+	postgis.ST_MakePolygon(
+		postgis.ST_SetSRID(
+			postgis.ST_GeomFromText(
+				format('LINESTRING(%s)', VARIADIC "coords")
+			)::postgis.geometry, 
+	4326))) = 't')
+
+
+
+*/
+
+
+
+
+
+  
 }
