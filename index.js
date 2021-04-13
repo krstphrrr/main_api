a0954dae519b4873fd72cf100eaa2e23f0107d7e
Kris! <42949874+krstphrrr@users.noreply.github.com>
2021-04-13 10:42:24 -0600

    testing actions

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.use(helmet())
 // // app.use(express.raw({limit:1}))
 
 app.get('/', cors(),(req, res) => 
-  res.send('ldc api up. update:03-19-2021')
+  res.send('ldc api up. update:04-13-2021')
 )
 
 //routes 
